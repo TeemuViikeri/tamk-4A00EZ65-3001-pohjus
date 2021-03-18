2dfee12c4b200f670d1b58b696a4d2478ee2a6ff
TeemuViikeri <teemu.viikeri@tuni.fi>
2021-03-18 10:07:31 +0200

    Remove comments.

--- homework/2021-10/e07/Main.kt
+++ homework/2021-10/e07/Main.kt
@@ -1,15 +1,3 @@
-// Create traditional palindrome app. Ask a string and output if given value was a palindrome. Reverse the given string using for - loop. 
-
-// Tips for Strings
-
-// - Notice that accessing character in Kotlin, use the array syntax: `"hello"[0]` returns `h`
-// - Notice that length of String is int, not a method: `"hello".length`, not `"hello".length()`
-
-// Tips for Comparison
-
-// - In Kotlin, == is the same as `equals` - method in Java. In Kotlin === is the same than == in Java
-// - See ranges how to decrement
-
 fun main() {
 	val string : String? = readLine()?.trim()
 	if (string != null) println(isPalindrome(string))
